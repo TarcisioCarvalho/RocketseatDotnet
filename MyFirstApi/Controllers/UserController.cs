0cdf3c8f675995b253666566f54da68782140d4e
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2025-09-15 21:13:16 -0300

    Studing Get

---
+++ MyFirstApi/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        var users = new List<string> { "Alice", "Bob", "Charlie" };
+        return Ok(users);
+    }
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(typeof(string),StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string),StatusCodes.Status404NotFound)]
+    public IActionResult GetUserById([FromRoute] int id)
+    {
+        var user = $"User{id}";
+        if(id < 1 || id > 3)
+        {
+            return NotFound("User not found");
+        }
+        return Ok(user);
+    }
+}
