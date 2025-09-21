edc34ecebf3d1684cc16b44efa1bda2f9c0efa79
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2025-09-20 22:06:27 -0300

    Beggin with unit tests the first test

---
+++ CashFlow/tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Title = "Valid Title",
+            Description = "Valid Description",
+            Date = DateTime.Now.AddDays(-1),
+            Value = 100.00m,
+            PaymentType = PaymentType.creditCard
+        };
+        var result = validator.Validate(request);
+        Assert.True(result.IsValid);
+    }
+}
