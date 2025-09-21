5af2b3965df2acfd2c5010813f8b41ff2ce11427
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2025-09-20 21:32:51 -0300

    MiddlewareCulture

---
+++ CashFlow/src/CashFlow.API/Middleware/CultureMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace CashFlow.API.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var suportedCultures = CultureInfo.GetCultures(CultureTypes.AllCultures)
+            .ToList();
+
+        var cultureQuery = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var culture = new CultureInfo("pt-BR");
+
+        if (!string.IsNullOrWhiteSpace(cultureQuery)
+            && suportedCultures.Exists(c => c.Name.Equals(cultureQuery)))
+        {
+                culture = new CultureInfo(cultureQuery);
+        }
+        CultureInfo.CurrentCulture = culture;
+        CultureInfo.CurrentUICulture = culture;
+        await _next(context);
+    }
+}
