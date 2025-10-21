2e466252bcad1c963bdc9d1a649ff5fc0ea9d588
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2025-10-21 11:09:42 -0300

    Begin endpoint to return a pdf report.

--- CashFlow/src/CashFlow.API/Controllers/ReportController.cs
+++ CashFlow/src/CashFlow.API/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
@@ -9,7 +10,7 @@ public class ReportController : ControllerBase
 {
     [HttpGet("excel")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public async Task<IActionResult> GetExcel([FromQuery] DateOnly month, [FromServices] IGenerateExpensesReportExcelUseCase generateExpensesReportExcelUseCase)
     {
         byte[] fileContents = await generateExpensesReportExcelUseCase.Execute(month);
@@ -17,5 +18,15 @@ public class ReportController : ControllerBase
             return NotFound();
 
         return File(fileContents,MediaTypeNames.Application.Octet,"report.xlsx");
+    }
+    [HttpGet("pdf")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetPdf([FromQuery] DateOnly month, [FromServices] IGenerateExpensesReportPdfUseCase generateExpensesReportPdfUseCase)
+    {
+        byte[] fileContents = await generateExpensesReportPdfUseCase.Execute(month);
+        if (fileContents.Length == 0)
+            return NotFound();
+        return File(fileContents,MediaTypeNames.Application.Pdf,"report.pdf");
     }
 }
---
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -0,0 +1,19 @@
+﻿
+using CashFlow.Domain.Repositories.Expenses;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
+{
+    private readonly IExepensesReadOnlyRepository _exepensesReadOnlyRepository;
+    public GenerateExpensesReportPdfUseCase(IExepensesReadOnlyRepository exepensesReadOnlyRepository)
+    {
+        _exepensesReadOnlyRepository = exepensesReadOnlyRepository;
+    }
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        var expenses = await _exepensesReadOnlyRepository.FilterByMonth(month);
+        if(expenses.Count == 0)
+            return Array.Empty<byte>();
+        return [];
+    }
+}
