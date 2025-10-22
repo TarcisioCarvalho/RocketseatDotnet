0e326cab9afbe78ff057cf595acccfa618ddea8a
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2025-10-22 06:45:02 -0300

    Finish app

---
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -0,0 +1,33 @@
+﻿using PdfSharp.Fonts;
+using System.Reflection;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+public class ExpensesReportFontResolver : IFontResolver
+{
+    public byte[]? GetFont(string faceName)
+    {
+        var stream = ReadFontFile(faceName);
+        stream ??= ReadFontFile(FontHelper.DEFAULT_FONT);
+
+        var length = (int)stream!.Length;
+        var fontData = new byte[length];
+        stream.Read(buffer: fontData, 0, length);
+        return fontData;
+    }
+
+    public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
+    {
+        return new FontResolverInfo(familyName);
+    }
+
+    private Stream? ReadFontFile(string faceName)
+    {
+        // Obtém a referência ao assembly atual
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // Retorna o stream do arquivo de fonte embutido no assembly
+        return assembly.GetManifestResourceStream(
+            $"CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts.{faceName}.ttf"
+        );
+    }
+}
--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,5 +1,9 @@
 ﻿
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 using CashFlow.Domain.Repositories.Expenses;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
@@ -8,12 +12,81 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
     public GenerateExpensesReportPdfUseCase(IExepensesReadOnlyRepository exepensesReadOnlyRepository)
     {
         _exepensesReadOnlyRepository = exepensesReadOnlyRepository;
+        GlobalFontSettings.FontResolver = new Fonts.ExpensesReportFontResolver();
     }
     public async Task<byte[]> Execute(DateOnly month)
     {
         var expenses = await _exepensesReadOnlyRepository.FilterByMonth(month);
-        if(expenses.Count == 0)
+        if (expenses.Count == 0)
             return Array.Empty<byte>();
-        return [];
+        var document = CreateDocument(month);
+        var page = CreatePage(document);
+        var table = page.AddTable();
+        table.AddColumn();
+        table.AddColumn("300");
+
+        var row = table.AddRow();
+       
+
+        row.Cells[1].AddParagraph("Hey, CashFlow");
+        row.Cells[1].Format.Font = new Font
+        {
+            Name = FontHelper.RALEWAY_BLACK,
+            Size = 16
+        };
+        row.Cells[1].VerticalAlignment = MigraDoc.DocumentObjectModel.Tables.VerticalAlignment.Center;
+
+        var paragraph = page.AddParagraph();
+        paragraph.AddFormattedText($"Total Spend In {month:Y}", new Font { Name = FontHelper.RALEWAY_REGULAR, Size = 15});
+        paragraph.AddLineBreak();
+        paragraph.AddFormattedText($"{expenses.Sum(expense => expense.Value)} R$", new Font { Name = FontHelper.WORKSANS_BLACK, Size = 50});
+        return RenderDocument(document);
     }
+
+    private Document CreateDocument(DateOnly month)
+    {
+        var document = new Document();
+
+        document.Info.Title = $"EXPENSES FOR {month:Y}";
+        document.Info.Author = "CashFlow";
+
+        var style = document.Styles["Normal"];
+        style.Font.Name = FontHelper.RALEWAY_REGULAR;
+
+
+        return document;
+    }
+
+
+
+    private Section CreatePage(Document document)
+    {
+        var section = document.AddSection();
+        section.PageSetup = document.DefaultPageSetup.Clone();
+
+        section.PageSetup.PageFormat = PageFormat.A4;
+
+        section.PageSetup.LeftMargin = 40;
+        section.PageSetup.RightMargin = 40;
+        section.PageSetup.TopMargin = 80;
+        section.PageSetup.BottomMargin = 80;
+
+        return section;
+    }
+
+    private byte[] RenderDocument(Document document)
+    {
+        var renderer = new PdfDocumentRenderer
+        {
+            Document = document
+        };
+
+        renderer.RenderDocument();
+
+        using var file = new MemoryStream();
+        renderer.PdfDocument.Save(file);
+
+        return file.ToArray();
+    }
+
 }
