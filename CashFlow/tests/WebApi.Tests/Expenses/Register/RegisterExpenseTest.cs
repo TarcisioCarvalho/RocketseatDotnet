8d1bc69c90dc4126d102e6aeccd187df2752a7fb
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2025-11-16 23:45:59 -0300

    Begin with integration tests for RegisterExpense

---
+++ CashFlow/tests/WebApi.Tests/Expenses/Register/RegisterExpenseTest.cs
@@ -0,0 +1,27 @@
+﻿using CommonTestUtilities.Requests;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WebApi.Tests.Expenses.Register;
+public class RegisterExpenseTest : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _httpClient;
+    private const string METHOD_URL = "api/Expense";
+    public RegisterExpenseTest(CustomWebApplicationFactory factory)
+    {
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        var response = await _httpClient.PostAsJsonAsync(METHOD_URL, request);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        var body = await response.Content.ReadAsStreamAsync();
+        var result = await JsonDocument.ParseAsync(body);
+        Assert.Equal(request.Title, result.RootElement.GetProperty("title").GetString());
+    }
+}
