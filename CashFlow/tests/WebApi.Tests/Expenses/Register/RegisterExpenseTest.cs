c9eb077bb41a8025c4431463594f72ed0bf33a63
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2025-11-25 21:31:29 -0300

    Test Error

--- CashFlow/tests/WebApi.Tests/Expenses/Register/RegisterExpenseTest.cs
+++ CashFlow/tests/WebApi.Tests/Expenses/Register/RegisterExpenseTest.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
+using WebApi.Tests.InlineData;
 
 namespace WebApi.Tests.Expenses.Register;
 public class RegisterExpenseTest : IClassFixture<CustomWebApplicationFactory>
@@ -28,12 +29,22 @@ public class RegisterExpenseTest : IClassFixture<CustomWebApplicationFactory>
         var result = await JsonDocument.ParseAsync(body);
         Assert.Equal(request.Title, result.RootElement.GetProperty("title").GetString());
     }
-
-    public async Task Error_Title_Empty()
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Title_Empty(string cultureInfo)
     {
         var request = RequestRegisterExpenseJsonBuilder.Build();
         request.Title = string.Empty;
+
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureInfo));
         var response = await _httpClient.PostAsJsonAsync(METHOD_URL, request);
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+        var body = await response.Content.ReadAsStreamAsync();
+        var result = await JsonDocument.ParseAsync(body);
+
+        var errors = result.RootElement.GetProperty("errorMessages").EnumerateArray();
+
     }
 }
