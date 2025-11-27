18c3d8e627316be1092172bb30e81081e3f89811
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2025-11-26 23:25:58 -0300

    Refactoring.

---
+++ CashFlow/tests/WebApi.Tests/CashFlowClassFixture.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Tests;
+public class CashFlowClassFixture : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _httpClient;
+    public CashFlowClassFixture(CustomWebApplicationFactory factory)
+    {
+        _httpClient = factory.CreateClient();
+    }
+}
