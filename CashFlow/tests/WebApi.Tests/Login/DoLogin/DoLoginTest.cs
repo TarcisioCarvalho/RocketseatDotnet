fdf47f5049df17247a4d6969b4dc9439300a9198
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2025-11-28 22:22:53 -0300

    DoLoginTest Refactoring

--- CashFlow/tests/WebApi.Tests/Login/DoLogin/DoLoginTest.cs
+++ CashFlow/tests/WebApi.Tests/Login/DoLogin/DoLoginTest.cs
@@ -4,23 +4,19 @@ using CommonTestUtilities.Requests;
 using FluentAssertions;
 using System.Globalization;
 using System.Net;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using System.Text.Json;
 using WebApi.Tests.InlineData;
 
 namespace WebApi.Tests.Login.DoLogin;
-public class DoLoginTest : IClassFixture<CustomWebApplicationFactory>
+public class DoLoginTest : CashFlowClassFixture
 {
-    private readonly HttpClient _httpClient;
-    private const string METHOD_URL = "api/Login";
+    private const string requestUri = "api/Login";
     private string _email = string.Empty;
     private string _name = string.Empty;
     private string _password = string.Empty;
 
-    public DoLoginTest(CustomWebApplicationFactory factory)
+    public DoLoginTest(CustomWebApplicationFactory factory) : base(factory)
     {
-        _httpClient = factory.CreateClient();
         _email = factory.GetEmail();
         _name = factory.GetName();
         _password = factory.GetPassword();
@@ -34,27 +30,29 @@ public class DoLoginTest : IClassFixture<CustomWebApplicationFactory>
             Email = _email,
             Password = _password
         };
-        var response = await _httpClient.PostAsJsonAsync(METHOD_URL, request);
+        var response = await DoPost(requestUri: requestUri, request);
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+
         var body = await response.Content.ReadAsStreamAsync();
         var result = await JsonDocument.ParseAsync(body);
+
         result.RootElement.GetProperty("name").GetString().Should().Be(_name);
         result.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
     }
 
     [Theory]
     [ClassData(typeof(CultureInlineDataTest))]
-    public async Task Error_Invalid_Login(string cultureInfo)
+    public async Task Error_Invalid_Login(string culture)
     {
         var request = RequestJsonLoginBuilder.Build(null);
-        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureInfo));
-        var response = await _httpClient.PostAsJsonAsync(METHOD_URL, request);
+   
+        var response = await DoPost(requestUri: requestUri, request, culture: culture);
         response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
 
         var body = await response.Content.ReadAsStreamAsync();
         var result = await JsonDocument.ParseAsync(body);
         var errors = result.RootElement.GetProperty("errorMessages").EnumerateArray();
-        var expectedMessage = ResourceErrorsMessages.ResourceManager.GetString("INVALID_LOGIN", new CultureInfo(cultureInfo));
-       errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+        var expectedMessage = ResourceErrorsMessages.ResourceManager.GetString("INVALID_LOGIN", new CultureInfo(culture));
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
     }
 }
