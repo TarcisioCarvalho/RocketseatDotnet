2765b8fafdca39c23b68fc704651491b06c5f2a6
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2025-12-10 21:04:14 -0300

    feat: begin with tests for update expense

---
+++ CashFlow/tests/CommonTestUtilities/Repositories/ExpensesUpdateOnlyRepositoryBuilder.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Domain.Repositories.Expenses;
+using Moq;
+
+namespace CommonTestUtilities.Repositories;
+public class ExpensesUpdateOnlyRepositoryBuilder
+{
+    private readonly Mock<IExpensesUpdateOnlyRepository> _exepensesUpdateOnlyRepository;
+
+    public ExpensesUpdateOnlyRepositoryBuilder()
+    {
+        _exepensesUpdateOnlyRepository = new Mock<IExpensesUpdateOnlyRepository>();
+    }
+
+    public IExpensesUpdateOnlyRepository Build() => _exepensesUpdateOnlyRepository.Object;
+}
---
+++ CashFlow/tests/UseCases.Tests/Expenses/Update/UpdateExpenseUseCaseTest.cs
@@ -0,0 +1,27 @@
+﻿using CashFlow.Application.UseCases.Expenses.Update;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+
+namespace UseCases.Tests.Expenses.Update;
+public class UpdateExpenseUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var useCase = CreateUpdateUseCase(user);
+       // var result = useCase.Execute();
+    }
+    private IUpdateExpenseUseCase CreateUpdateUseCase(User user)
+    {
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var mapper = MapperBuilder.Build();
+        var repository = new ExpensesUpdateOnlyRepositoryBuilder().Build();
+
+        return new UpdateExpenseUseCase(expenseRepository: repository, unitOfWork: unitOfWork, mapper: mapper, loggedUser:loggedUser);
+    }
+}
