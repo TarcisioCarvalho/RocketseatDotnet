4f0aa9dc2881c20012e21d472ca2294249763269
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2026-01-01 14:41:45 -0300

    feat: begin integration tests

---
+++ CashFlow/tests/WebApi.Tests/Users/Update/UpdateUserTest.cs
@@ -0,0 +1,22 @@
+﻿using CommonTestUtilities.Requests;
+using System.Net;
+using System.Text.Json;
+
+namespace WebApi.Tests.Users.Update;
+public class UpdateUserTest : CashFlowClassFixture
+{
+    private const string requestUri = "api/User";
+    private string _token = string.Empty;
+    public UpdateUserTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _token = factory.UserTeamMember.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestUpdateUserJsonBuilder.Build();
+        var response = await DoPut(requestUri, request, _token);
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    }
+}
