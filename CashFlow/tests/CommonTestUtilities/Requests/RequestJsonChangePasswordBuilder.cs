ed4420d644b36f8cf7b50beafd8bde879bd991f9
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2026-01-03 14:11:27 -0300

    feat :  change password validator  unittest

---
+++ CashFlow/tests/CommonTestUtilities/Requests/RequestJsonChangePasswordBuilder.cs
@@ -0,0 +1,13 @@
+﻿
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestJsonChangePasswordBuilder
+{
+    public static RequestChangePasswordJson Build()
+    {
+        return new Bogus.Faker<RequestChangePasswordJson>().
+          RuleFor(user => user.Password, faker => faker.Internet.Password()).
+          RuleFor(user => user.NewPassword, (faker, user) => faker.Internet.Password(length: 8, prefix: "!Aa1"));
+    }
+}
---
+++ CashFlow/tests/Validators.Tests/Users/ChangePassword/ChangePasswordValidatorTest.cs
@@ -0,0 +1,26 @@
+﻿using CashFlow.Application.UseCases.Users.ChangePassword;
+using CommonTestUtilities.Requests;
+
+namespace Validators.Tests.Users.ChangePassword;
+public class ChangePasswordValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var request = RequestJsonChangePasswordBuilder.Build();
+        var result = new ChangePasswordValidator().Validate(request);
+        Assert.True(result.IsValid);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void Error_Password_Empty(string password)
+    {
+        var request = RequestJsonChangePasswordBuilder.Build();
+        request.NewPassword = password;
+        var result = new ChangePasswordValidator().Validate(request);
+        Assert.False(result.IsValid); 
+    }
+}
