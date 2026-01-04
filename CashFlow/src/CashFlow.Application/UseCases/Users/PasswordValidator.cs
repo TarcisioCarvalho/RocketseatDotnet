c1329c0b7137c8f92319e7d0776d11b9893833cb
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2026-01-04 20:28:24 -0300

    feat : implements unit tests for changePassword useCase

--- CashFlow/src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
@@ -11,37 +11,37 @@ public class PasswordValidator<T> : PropertyValidator<T, string>
         if (string.IsNullOrWhiteSpace(password))
         {
             context.AddFailure("Password is required.");
-            return false;
+            return true;
         }
 
         if (password.Length < 8)
         {
             context.AddFailure("Password must be at least 8 characters long.");
-            return false;
+            return true;
         }
 
         if (!password.Any(char.IsUpper))
         {
             context.AddFailure("Password must contain at least one uppercase letter.");
-            return false;
+            return true;
         }
 
         if (!password.Any(char.IsLower))
         {
             context.AddFailure("Password must contain at least one lowercase letter.");
-            return false;
+            return true;
         }
 
         if (!password.Any(char.IsDigit))
         {
             context.AddFailure("Password must contain at least one digit.");
-            return false;
+            return true;
         }
 
         if (!password.Any(ch => !char.IsLetterOrDigit(ch)))
         {
             context.AddFailure("Password must contain at least one special character.");
-            return false;
+            return true;
         }
 
         return true;
---
+++ CashFlow/tests/UseCases.Tests/Users/ChangePassword/ChangePasswordUseCaseTest.cs
@@ -0,0 +1,45 @@
+﻿using CashFlow.Application.UseCases.Users.ChangePassword;
+using CashFlow.Domain.Entities;
+using CashFlow.Exception.ExceptionBase;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Security;
+
+namespace UseCases.Tests.Users.ChangePassword;
+public class ChangePasswordUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var request = RequestJsonChangePasswordBuilder.Build();
+        var useCase = CreateUseCase(user, request.Password);
+        await useCase.Execute(request);
+    }
+
+    [Fact]
+    public async Task Error_New_Password_Empty()
+    {
+        var user = UserBuilder.Build();
+        var request = RequestJsonChangePasswordBuilder.Build();
+        request.NewPassword = string.Empty;
+        var useCase = CreateUseCase(user, request.Password);
+        var act = async () => await useCase.Execute(request);
+        var exception = await Assert.ThrowsAsync<ErrorOnValidationException>(() => act());
+        Assert.Single(exception.GetErrors());
+        Assert.Equal("Password is required.", exception.GetErrors().First());
+
+        // Password is required.
+    }
+    private IChangePasswordUseCase CreateUseCase(User user, string? password = null)
+    {
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var userUpdateRepository = new UserUpdateOnlyRepositoryBuilder().GetById(user.Id, user).Build();
+        var passwordEncripter = new PasswordEncripterBuilder().Verify(password).Build();
+
+        return new ChangePasswordUseCase(loggedUser, passwordEncripter, userUpdateRepository, unitOfWork);
+    }
+}
