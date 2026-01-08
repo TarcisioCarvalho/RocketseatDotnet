c733ec529c47b8eadec2864a101e6b24ddbcd8e1
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2026-01-07 22:01:52 -0300

    feat :  unit test for delete useCase

---
+++ CashFlow/tests/UseCases.Tests/Users/Delete/DeleteUserAccountUseCaseTest.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Application.UseCases.Users.Delete;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+
+namespace UseCases.Tests.Users.Delete;
+public class DeleteUserAccountUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+        await useCase.Execute();
+    }
+    private IDeleteUserAccountUseCase CreateUseCase(User user)
+    {
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var repository = UserWriteOnlyRepositoryBuilder.Build();
+
+        return new DeleteUserAccountUseCase(loggedUser, repository, unitOfWork);
+    }
+}
