ac6b0fc2d79a52a9a485c681d44a4426cbd45d8d
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2026-01-08 23:37:27 -0300

    feat: integration test for delete user

---
+++ CashFlow/tests/WebApi.Tests/Users/Delete/DeleteUserTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace WebApi.Tests.Users.Delete;
+public class DeleteUserTest : CashFlowClassFixture
+{
+    private readonly string _token = string.Empty;
+    private const string requestUri = "api/User";
+    public DeleteUserTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _token = factory.UserTeamMember.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+       var response = await DoDelete(requestUri: requestUri, token: _token);
+       Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    }
+}
